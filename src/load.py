d5fdbdfc253aa50e2a3d9beb58c9c27084c05550
Victor.calle <victor.calle@est.iudigital.edu.co>
2025-03-02 15:51:40 -0500

    comentar load

--- src/load.py
+++ src/load.py
@@ -16,5 +16,11 @@ def load(data_frames: Dict[str, DataFrame], database: Engine):
     # como una tabla.
     # Para el nombre de la tabla, utiliza las claves del diccionario `data_frames`.
     
+    # Itera sobre cada par (nombre de la tabla, DataFrame) en el diccionario data_frames.
     for table_name, df in data_frames.items():
+    # Guarda el DataFrame en la base de datos:
+    # - table_name: se usa como nombre de la tabla.
+    # - database: es la conexión al motor de la base de datos.
+    # - if_exists='replace': reemplaza la tabla si esta ya existe.
+    # - index=False: no guarda el índice del DataFrame como columna en la tabla.
         df.to_sql(table_name, database, if_exists='replace', index=False)
