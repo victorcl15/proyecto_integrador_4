16cde27c7a6d89a0b129b74e4532a148f157f58f
Victor.calle <victor.calle@est.iudigital.edu.co>
2025-04-01 22:47:29 -0500

    correcion query revenue_per_state

--- queries/revenue_per_state.sql
+++ queries/revenue_per_state.sql
@@ -2,19 +2,17 @@
 -- La primera contendrá las abreviaturas que identifican a los 10 estados con mayores ingresos,
 -- y la segunda mostrará el ingreso total de cada uno.
 -- PISTA: Todos los pedidos deben tener un estado "delivered" y la fecha real de entrega no debe ser nula.
-SELECT 
-    c.customer_state,
-    SUM(oi.price + oi.freight_value) AS Revenue
-FROM olist_orders AS o
-JOIN olist_customers AS c 
-    ON o.customer_id = c.customer_id
-JOIN olist_order_items AS oi 
-    ON o.order_id = oi.order_id
-WHERE 
-    o.order_status = 'delivered' 
-    AND o.order_delivered_customer_date IS NOT NULL
-GROUP BY 
-    c.customer_state
-ORDER BY 
-    Revenue DESC
-LIMIT 10;
+SELECT
+    oc.customer_state,
+    SUM(oop.payment_value) AS Revenue
+FROM olist_customers oc
+LEFT JOIN olist_orders oo 
+    ON oc.customer_id = oo.customer_id
+LEFT JOIN olist_order_payments oop 
+    ON oo.order_id = oop.order_id
+WHERE oo.order_status = 'delivered'
+  AND oo.order_delivered_customer_date IS NOT NULL
+GROUP BY oc.customer_state
+ORDER BY Revenue DESC
+LIMIT 10;
+
